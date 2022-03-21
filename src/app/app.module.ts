68cbdedee14483d242281de1caca76d882ce7f1c
Jett <premiumconsulting@jettdembin.dev>
2022-03-21 10:06:10 -0400

    restart app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ReciipesComponent } from './reciipes/reciipes.component';
+import { RecipesComponent } from './recipes/recipes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
+    ReciipesComponent,
+    RecipesComponent,
   ],
   imports: [
     BrowserModule,
