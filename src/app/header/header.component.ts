d7d01c02d2b00171193ffcf83b4c36c88e06922d
Jett <premiumconsulting@jettdembin.dev>
2022-03-22 13:11:56 -0400

    added navigation without Routing

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,16 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  @Output() featureSelected = new EventEmitter<string>();
+
   collapsed = true;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onSelect(feature: string) {
+    this.featureSelected.emit(feature);
   }
-
 }
