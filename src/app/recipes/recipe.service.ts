875e6429321a93bcf855f97c3386b4ee38091a9c
Jett <premiumconsulting@jettdembin.dev>
2022-03-28 14:11:10 -0400

    import to shopping list working

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,23 +1,34 @@
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 import { Recipe } from './recipe.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
     new Recipe(
-      'A Test Recipe',
-      'This is a test',
-      'https://www.foodista.com/sites/default/files/IMG_1363a.jpg'
+      'Steak n Fries',
+      'Your Favorite Steak n Fries',
+      'https://www.foodista.com/sites/default/files/IMG_1363a.jpg',
+      [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
     ),
     new Recipe(
-      'Another Test Recipe',
-      'This is a test',
-      'https://www.foodista.com/sites/default/files/IMG_1363a.jpg'
+      'Burger',
+      'A Tasty Burger',
+      'https://www.foodista.com/sites/default/files/IMG_1363a.jpg',
+      [new Ingredient('Buns', 2), new Ingredient('Meat', 1)]
     ),
   ];
 
+  constructor(private slService: ShoppingListService) {}
+
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
   }
 }
